24b3ff87f7225fc284ae9556e05784d1d881def7
gregthomson0550 <129339510+gregthomson0550@users.noreply.github.com>
2024-02-26 17:33:22 +0000

    Init

---
+++ utils.js
@@ -0,0 +1,25 @@
+
+var EasyUtils = (function () {
+    // Private helper function
+    function add(a, b) {
+      return a + b;
+    }
+  
+    // Public functions
+    return {
+      // Function to add two numbers
+      addNumbers: function (num1, num2) {
+        return add(num1, num2);
+      },
+  
+      // Function to capitalize the first letter of a string
+      capitalize: function (str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+      },
+  
+      // Function to check if a number is even
+      isEven: function (num) {
+        return num % 2 === 0;
+      }
+    };
+  })();
